f9768543e4803c91db6d9992a167c1d18a681704
rishikmishra7 <rishikmishratech@gmail.com>
2025-01-18 18:56:02 +0530

    Added more buttons to the APP

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ export default function Navbar(props) {
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-primary" type="submit">Search</button>
       </form>
       </div>
       </div>
@@ -33,5 +33,5 @@ export default function Navbar(props) {
 Navbar.propTypes = {
     title :PropTypes.string,
 
-                    aboutText :PropTypes.string
+    aboutText :PropTypes.string
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,56 @@
 import React,{useState} from 'react'
 export default function TextForm(props) {
 const handleUpclick =()=>{
-    console.log("Uppercase was clicked " +text)
+    //console.log("Uppercase was clicked " +text)
     let newText =text.toUpperCase();
     setText(newText);
 }
+const handleLoclick =()=>{
+  //console.log("Uppercase was clicked " +text)
+  let newText =text.toLowerCase();
+  setText(newText);
+}
+const handleClearclick =()=>{
+  //console.log("Uppercase was clicked " +text)
+  let newText =("");
+  setText(newText);
+}
+const handleCopyclick = () => {
+  navigator.clipboard.writeText(text).then(() => {
+    alert("Text copied to clipboard!");
+  });
+};
 const handleOnChange =(event)=>{
-    console.log("On Change")
+  //  console.log("On Change")
     setText(event.target.value);
 }
 
-    const [text, setText] = useState('Enter Text Here');
+    const [text, setText] = useState('');
 
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading} </h1>
      <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpclick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleUpclick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLoclick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleClearclick}>Clear</button>
+<button className="btn btn-primary mx-1" onClick={handleCopyclick}>Copy</button>
+
+
+
     </div>
+    <div className="container my-3">
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read </p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    
+    </div>
+    </>
+
   )
 }
